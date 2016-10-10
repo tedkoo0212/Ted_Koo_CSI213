8720f4212f136c5b09c40ce8fda004d4a624a9a3
Ted Koo <jkoo@albany.edu>
2016-10-09 20:22:44 -0400

    Create VarNode.java

---
+++ VarNode.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+//  LinkedList.Node;
+
+public class VarNode {
+    // Java program to find middle of linked list
+        Node head; // head of linked list
+
+        /* Linked list node */
+        class Node
+        {
+            int data;
+            Node next;
+            Node(int d)
+            {
+                data = d;
+                next = null;
+            }
+        }
+        
+        public void insert() // needs to be in an alphabetical order
+        {
+        	
+        }
+        
+        public void delete()  // deleting only the single node value
+        {
+        	
+        }
+        
+        public void trv_forw()  // traversing the list forward and print at end
+        {
+        }
+        
+        public void trv_back() // traversing the list backward and print at end
+
+        {
+        }
+        
+        public void destroy() // destroy the whole linked list.
+        {
+        }
+        }
+        
+        
+        
+}
